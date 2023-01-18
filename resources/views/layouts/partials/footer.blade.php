c1e4e210a11a7337509370107d82d51ef594405c
dev6 sitebuilder <dev6.sitebuilder@gmail.com>
2023-01-18 21:38:52 +0330

    updated footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <!-- Footer Location-->
-            <div class="col-lg-4 mb-5 mb-lg-0">
+            <div class="col-lg-3 mb-5 mb-lg-0">
                 <h4 class="text-uppercase mb-4">{{ __('Location') }}</h4>
                 <p class="lead mb-0">
                     {{ __('2215 John Daniel Drive') }}
@@ -10,8 +10,16 @@
                     {{ __('Clark, MO 65243') }}
                 </p>
             </div>
+            <div class="col-lg-3 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">{{ __('Contact') }}</h4>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">{{ __('Tel') }}: +39 (0)40 2443 2128</li>
+                    <li class="list-group-item">{{ __('Fax') }}: +39 (0)40 2443 2128</li>
+                    <li class="list-group-item">{{ __('Email') }}: info(a)persianfoodproducts.com</li>
+                </ul>
+            </div>
             <!-- Footer Social Icons-->
-            <div class="col-lg-4 mb-5 mb-lg-0">
+            <div class="col-lg-3 mb-5 mb-lg-0">
                 <h4 class="text-uppercase mb-4">{{ __('Around the Web') }}</h4>
                 <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
                 <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
@@ -19,7 +27,7 @@
                 <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
             </div>
             <!-- Footer About Text-->
-            <div class="col-lg-4">
+            <div class="col-lg-3">
                 <h4 class="text-uppercase mb-4">{{ __('About Persian Classic Product') }}</h4>
                 <p class="lead mb-0">
                     {{ __('Persian Classic Product is a online shop, products for persian all around the world.') }}
