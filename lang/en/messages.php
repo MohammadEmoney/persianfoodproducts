e2b46d48133e5150fc3ab01ecc14129f9d3bf3a9
dev6 sitebuilder <dev6.sitebuilder@gmail.com>
2023-01-18 22:38:27 +0330

    updated about message

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'about' => 'Persian Classic Product is Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus temporibus possimus rem! Asperiores dolor eaque eum odio, officiis nam delectus ea optio enim assumenda expedita, magni odit consequuntur repudiandae. Vitae!',
-    'about_2' => 'You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!',
+    // 'about' => 'Persian Classic Product is Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus temporibus possimus rem! Asperiores dolor eaque eum odio, officiis nam delectus ea optio enim assumenda expedita, magni odit consequuntur repudiandae. Vitae!',
+    'about' => 'Mahdi Rashvand Avaei was PhD graduated from School of Agriculture, Forestry, Food and Environmental Science, University of Basilicata. He is expert in food engineering and he published several scientific papers in the high qualified journal. During the master period he worked on the qualification and discrimination of the edibles oils such as olive oil using spectroscopy and data mining methods.',
+    'about_2' => 'Also, during the PhD course he investigated the modified atmosphere packaging (MAP) to enhance the shelf life of the fresh fruits and vegetables. Currently he is the expert and business man of the Negin Persia Company and who can answer to the consumer’s question. In the following you can find his contact details.',
     'address_1' => 'Headquarter:N.96, Azimie district, Karaj city, Alborz province, Iran',
     'address_2' => 'Italy branch: N.33, Via Vescovado, Potenza city,Basilicata Province, Italy',
-    
+
 ];
