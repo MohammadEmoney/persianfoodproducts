67edbddeac2e4868b6f3ba39e918d61b02948760
dev6 sitebuilder <dev6.sitebuilder@gmail.com>
2023-03-09 23:40:15 +0330

    added comment model

---
+++ app/Models/Comment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'commentable',
+        'body',
+        'user_id',
+        'is_approved',
+    ];
+
+    /**
+     * @return MorphTo
+     */
+    public function commentable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
